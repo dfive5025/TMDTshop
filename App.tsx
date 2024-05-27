ecc86202e8d58c9021f9445b76c8c0ca4fba13d6
dat nguyen <nguyentuandat2809@gmail.com>
2024-05-27 09:30:42 +0700

    fix splash screen done

--- App.tsx
+++ App.tsx
@@ -1,23 +1,35 @@
-import { View, Text } from "react-native";
-import React, { useEffect, useState } from "react";
-import { SplashScreen } from "./src/screens";
-import AuthNavigator from "./src/navigators/AuthNavigator";
-import { NavigationContainer } from "@react-navigation/native";
+import {View, Text, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SplashScreen} from './src/screens';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  const[isShownSplash, setIsShowSplash] = useState(true);
+  const [isShownSplash, setIsShowSplash] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     const timeout = setTimeout(() => {
-      setIsShowSplash(false)
+      setIsShowSplash(false);
     }, 1500);
 
     return () => clearTimeout(timeout);
-  }, [])
+  }, []);
 
-  return isShownSplash ? <SplashScreen /> : <NavigationContainer>
-    <AuthNavigator />
-  </NavigationContainer>
-}
+  return (
+    <>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor="transparent"
+        translucent></StatusBar>
+      {isShownSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      )}
+    </>
+  );
+};
 
 export default App;
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
+import React from 'react';
+import {appInfo} from '../constants/appInfos';
+import {SpaceComponent} from '../components';
+import {appColors} from '../constants/appColors';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      source={require('../assets/images/splash-img.png')}
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+      imageStyle={{flex: 1}}>
+      <Image
+        source={require('../assets/images/demo-event-image.png')}
+        style={{
+          width: appInfo.size.WIDTH * 0.8,
+          resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={16}></SpaceComponent>
+      <ActivityIndicator color={appColors.gray} size={22} />
+    </ImageBackground>
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
