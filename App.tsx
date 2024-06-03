fd3909485e0709a2501e982cabea2996d2347aa6
dat nguyen <nguyentuandat2809@gmail.com>
2024-06-03 08:21:54 +0700

    add flash screen

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import React, {useEffect, useState} from 'react';
 import {SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
   const [isShownSplash, setIsShowSplash] = useState(true);
+
+  const [accessToken, setAccessToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('assetToken');
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -14,6 +19,17 @@ const App = () => {
 
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await getItem();
+
+    console.log(token);
+    token && setAccessToken(token);
+  };
 
   return (
     <>
@@ -25,7 +41,7 @@ const App = () => {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       )}
     </>
--- src/components/SpaceComponent.tsx
+++ src/components/SpaceComponent.tsx
@@ -1,23 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 interface Props {
-    width?: number;
-    height?: number;
+  width?: number;
+  height?: number;
 }
 
 const SpaceComponent = (props: Props) => {
   const {width, height} = props;
 
-  
-    return (
-    <View 
-    style={{
-        width,height,
-    }}>
-      
-    </View>
-  )
-}
+  return (
+    <View
+      style={{
+        width,
+        height,
+      }}></View>
+  );
+};
 
-export default SpaceComponent
+export default SpaceComponent;
